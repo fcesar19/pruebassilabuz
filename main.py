de3fa6cea4a3da988fddfa81589946397a51398d
Cesar Flores <107594589+fcesar19@users.noreply.github.com>
2022-09-17 23:51:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import math
+from math import *
+
+#iniciar=True
+#arreglo=[]
+#while iniciar == True:
+  #x=input("Ingrese el primero numero")
+  #arreglo.append(x)
+  #y=input("Ingrese el segundo numero")
+  #arreglo.append(y)
+  #max_values=max(arreglo)
+  #print(f"Maximo valor {max_values}")
+
+#palabra=input("ingrese una palabra: ")
+#intentos=int(input("Ingrese numero de veces que se repetira: "))
+#while(intentos!=0):
+  #print(palabra)
+  #intentos-=1
+
+#falta
+#x=0
+#while(x<=10):
+  #if(x==3):
+    #print("BOOM!")
+  #else:
+    #print(x)
+  #x+=1
+
+#x=int(input("Ingrese cantidad de filas: "))
+#y=0
+#while(y<=x):
+ #z = 0
+ #while z < y:
+  #print("+" ,end="")
+  #z=z+1
+ #print("")
+ #y+=1
+
+#print("-----CALCULADORAA----")
+#print("1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir")
+#numero=int(input("Ingrese el primer numero: "))
+#numero2=int(input("Ingrese el segundo numero: "))
+#eleccion=int(input("Ingrese la opcion a realizar: "))
+#while eleccion not in (1,2,3,4):
+  #print("Elija entre las opciones")
+  #eleccion=int(input("Su respuesta"))
+#if eleccion==1:
+  #resultadosuma=numero+numero2
+  #print("la suma es ",resultadosuma)
+#elif eleccion==2:
+  #resultadorestar=numero-numero2
+  #print("la resta es ",resultadorestar)
+#elif eleccion==3:
+  #resultadomulti=numero*numero2
+  #print("la multiplicacion es ",resultadomulti)
+#elif eleccion==4:
+  #resultadodividir=numero/numero2
+  #print("la division es ",resultadodividir)
+#else:
+  #print("Pelotudo")
+
